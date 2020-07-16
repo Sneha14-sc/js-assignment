1c1d62d2fc6fb84124eead3c2084bd03f35ee2b6
Sneha14-sc <67182479+Sneha14-sc@users.noreply.github.com>
2020-07-16 12:35:37 +0530

    Add files via upload

---
+++ Assignment-day2/scripts/age.js
@@ -0,0 +1,10 @@
+console.log('Question 3');
+let age=prompt("Enter your age");
+if(age>=21)
+{
+    console.log("You can drink.");
+}
+else
+{
+    console.log("You can't drink.");
+}
---
+++ Assignment-day2/scripts/input.js
@@ -0,0 +1,4 @@
+console.log("Question 1");
+
+let color=prompt("What's your favourite color?");
+console.log(`Your Favourite color is:${color}`);
